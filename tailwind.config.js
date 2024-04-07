05abef4390310966326040a3731bbd539f7ab7c9
maya <eng.maya.esmaeel1@gmail.com>
2024-04-07 12:36:16 -0700

    base style

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        primaryColor: '#010a5e',
+        primaryColorLight: "#010d78",
+        secondaryColor: '#FFCC00',
+        whiteColor: '#fff',
+        blackColor: "#000",
+        greenLigthColor: "#C2EFD4",
+        greenColor: "#224F34",
+        paragraphColor : '#267D49',
+        imageColor : '#6BC785',
+
+        
+
+        
+        redColor: "#cc3433",
+        darkColor: '#000',
+        darkColorLight: "#171717",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '1.5rem'
+        }
+      },
+    },
+  },
+  plugins: [],
+}
+
